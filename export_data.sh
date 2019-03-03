99f28189586c1bcd148e46e89299af22cadd9a2f
Feng Li <feng.li@greencap.com.au>
2019-03-03 12:05:28 +1100

    add 2 parameters

--- export_data.sh
+++ export_data.sh
@@ -4,11 +4,16 @@ source potatos.credential
 #echo ${SOURCE_DB_USERNAME}
 #echo ${SOURCE_DB_PASSWORD}
 
-FILE=potatos.conf
+# 1st parameter: potatos.conf 
+# The databases are going to be dumped, in the format of one database one line
+# 2nd parameter: $dumpath 
+# The location to dump the databases
+
+FILE=$1
 while read LINE
     do { 
         echo "Dumping database ${LINE}"
-        mysqldump -h ${SOURCE_DB_HOST} -u ${SOURCE_DB_USERNAME} -p${SOURCE_DB_PASSWORD} ${LINE} --databases > ${LINE}_db.dump
+        mysqldump -h ${SOURCE_DB_HOST} -u ${SOURCE_DB_USERNAME} -p${SOURCE_DB_PASSWORD} ${LINE} --databases > ${2}/${LINE}_db.dump
     }
 done < ${FILE}
 
