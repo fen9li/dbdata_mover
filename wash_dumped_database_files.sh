a3f5be9c71c6f42b785e7014af4dedbaa479affe
Feng Li <feng.li@greencap.com.au>
2019-03-20 13:56:31 +1100

    refectoring wash_dumped_database_files.sh

--- wash_dumped_database_files.sh
+++ wash_dumped_database_files.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-source potatos.credential
-#echo ${TARGET_DB_HOST}
-#echo ${TARGET_DB_USERNAME}
-#echo ${TARGET_DB_PASSWORD}
-
-[ -z ${TARGET_DB_HOST} ] && echo "TARGET_DB_HOST is Empty" && exit 1
-[ -z ${TARGET_DB_USERNAME} ] && echo "TARGET_DB_USERNAME is Empty" && exit 1
-[ -z ${TARGET_DB_PASSWORD} ] && echo "TARGET_DB_PASSWORD is Empty" && exit 1
-
-mysql -h ${TARGET_DB_HOST} -u ${TARGET_DB_USERNAME} -p${TARGET_DB_PASSWORD} -e exit > /dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-    echo "User ${TARGET_DB_USERNAME} can not connect to ${TARGET_DB_HOST} ..."
-    exit 2
-fi
 
 if [ "$#" -lt 2 ]; then
     echo "2 parameters required"
